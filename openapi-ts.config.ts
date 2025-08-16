f9ce161bbbd620fc19f92fba9b2c5875a4010024
Jaye Hackett <jaye.hackett@gmail.com>
2025-08-16 18:10:42 +0100

    clean out

--- openapi-ts.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "@hey-api/openapi-ts"
-
-export default defineConfig({
-  input: "https://app.vouchsafe.id/openapi/swagger.json",
-  output: "src/gen",
-  plugins: ["@hey-api/client-fetch"],
-})
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./VouchsafeClient"
-// optionally: export * from './gen' if you want to expose raw ops too
